2a63db535e0da837e7848fea03458ec7abb3522a
benwin22 <145385526+benwin22@users.noreply.github.com>
2023-12-04 09:37:05 -0600

    code revamp

--- dist/index.js
+++ dist/index.js
@@ -1,58 +1,50 @@
 "use strict";
-function testMe(name) {
-    console.log(name);
+Object.defineProperty(exports, "__esModule", { value: true });
+const uuid_1 = require("uuid");
+class Item {
+    constructor(name, price, description) {
+        this.id = (0, uuid_1.v4)();
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
 }
-testMe("Here's Johnny");
-// Continue on with your RPG character guys
-// This one is a long one so buckle up!
-// Step 1: Create A Typescript Project
-// Create a node project by intializing npm your in project's folder
-// Make it a Typescript Project by install TypeScript
-// Configure your .tsconfig to align with our recommended options for this project
-// Use npm to install uuid and the uuid types
-// Step 2: Create Some Types/Classes/Objects
-// Create a Type to describe a Item
-// id as a string
-// name as a string
-// price as an number
-// description as a string
-// Create a Type to describe a User/Classes/Objects
-// id as a string
-// name as a string
-// age as an number
-// cart as an array of Items
-// Step 3: Create some function
-// createUser
-// this function will return an object of type User. It will autogenerate an UUID for the id. It will require name, and age to be passed in as arguments. It will also initialize an empty cart.
-// createItem
-// this function will return an object of type Item. It will autogenerate an UUID for the id. It will require name, price, and description to be passed in as arguments.
-// addToCart
-// this function will bring an object of Item Type and an User object and it will add the item to the users cart
-// removeFromCart
-// this function will bring an object of Item Type and an User object and it will remove all the instances of the item to the users cart (so the cart would have zero of these items left)
-// EXTRA CREDIT: removeQuantityFromCart
-// this function will bring an object of Item Type and an User object and a quantity of the item to remove and it will remove the quantity amount of instances of the item to the users cart (so if the cart had 5 red hats and we pass inthe red hat item and the number 3 for the quantitiy we would end up with 2 red hats left in the cart)
-// EXTRA CREDIT: cartTotal
-// this function will calculate the total price of all items in our cart and RETURNS that value
-// printCart
-// this function will take a user and console log the items in the users cart
-// Step 4: Create Driver Code to emulate a front end user
-// use the functions created to accomplish these tasks in order
-// Create a User
-// Create 3 Items to Sell. For this example we will call them Item A Item B and Item C (you can name them anything that make sense)
-// Add Item A to the users Cart
-// print the contents of the user's cart
-// print the Total of the user's cart
-// Add 3 Items to the users Cart
-// print the contents of the user's cart
-// print the Total of the user's cart
-// Use your remove (not remove by Quantity function) to remove all of Items from your cart
-// print the contents of the user's cart
-// print the Total of the user's cart
-// Verify all functions work as expected, if they don't figure out why not and fix your functions.
-// you only need to do one of these
-// npm init -- initializes node packages
-// npm init -y <-- says yes to everything and is fast
-// tsc -init
-// update package.json with start scripts:
-// "tsc && node ./dist/index.js",
+class User {
+    constructor(name, age) {
+        this.id = (0, uuid_1.v4)();
+        this.name = name;
+        this.age = age;
+        this.cart = [];
+    }
+    addToCart(item) {
+        this.cart.push(item);
+    }
+    printCart() {
+        console.log(this.name, this.cart);
+    }
+    //                            removed: quantity in below arguement bc need counter for cart
+    removeItemFromCart(item) {
+        this.cart = this.cart.filter((cartItem) => cartItem.id !== item.id);
+    }
+}
+const milk = new Item('Milk', 3.99, 'Whole Milk');
+const apples = new Item('Apples', 5.99, '3lbs of Pink Lady Apples');
+const eggs = new Item('Eggs', 7.99, 'Free Range Farm Eggs');
+const bread = new Item('Bread', 4.99, 'Sourdough');
+const user1 = new User('Beth Ross', 25);
+const user2 = new User('Jack Black', 55);
+// need to add quantity counter in cell above, then add extra argument to below
+user1.addToCart(milk);
+user1.addToCart(bread);
+// user1.addToCart(apples);
+user1.addToCart(eggs);
+user1.printCart();
+user2.addToCart(milk);
+user2.addToCart(apples);
+user2.addToCart(bread);
+user2.printCart();
+user1.removeItemFromCart(eggs);
+user1.printCart();
+// need a way to have quantity
+// user1.removeQuantityFromCart(eggs);
+// user1.printCart();
--- src/index.ts
+++ src/index.ts
@@ -1,160 +1,75 @@
-function testMe(name:string):void{
-    console.log(name)
-}
+import { v4 as uuidv4 } from 'uuid';
 
-testMe("Here's Johnny")
 
-type Items = {
-    id: string,
-    name: string,
-    price: number,
-    description: string
-}
-
-let item:[id:string, name:string, price:number, description:string]
-
-class Items {
-    id: string
-    name: string
-    price: number
-    description: string
+class Item implements Item {
+    id: string;
+    name: string;
+    price: number;
+    description: string;
+   
     
-    constructor(id:string,name:string,price:number,description:string){
-        this.id = id
-        this.name = name
-        this.price = price
-        this.description = description
-    }
-}
-let item1={
-    id:"8976-0",
-    name:"apples",
-    price:5.99,
-    description: "3lbs pink lady apples"
-}
-console.log(item1)
-let item2={
-    id:"8432-0",
-    name:"bread",
-    price:3.99,
-    description: "scratch-made sourdough"
-}
-console.log(item2)
-let item3={
-    id:"4321-0",
-    name:"milk",
-    price:2.99,
-    description: "1/2 gallon Whole Milk"
-}
-console.log(item3)
-type User = {
-    id: string,
-    name: string,
-    age: number,
-    cart: string[];
-}
-let cart: [item:string, quantity:string, total:number]
-// enum?
-let user1 ={
-    id:"shopper-001",
-    name:"Beth",
-    age:30,
-    cart:[item1, item2, item3]
-}
-type Cart = {
-    cartId:string,
-    cartItems:string [],
-    cartQuantity:number,
-    cartTotal: string[]
-}
-let cart1 = {
-    cartId:"cart-01",
-    cartItems:["milk", "apples"],
-    cartQuantity:2,
-    cartTotal: ["item3"+"item1"]
+
+  constructor(name: string, price: number, description: string) {
+    this.id = uuidv4();
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    
+  }
 }
 
-type newUser ={
-    name:string,
-    age:number
-}
+class User implements User {
+    id: string;
+    name: string;
+    age: number;
+    cart: Item[];
 
-class newUser{
-    name(){console.log("name")}
-    age(){console.log("age")}
-}
-function createUser(newuser:unknown){
-    if (newuser instanceof newUser){
-        newuser.name()
-        newuser.age()
-    }else{
-        console.log("please enter name and age")
-    }
-}
+  constructor(name: string, age: number) {
+    this.id = uuidv4();
+    this.name = name;
+    this.age = age;
+    this.cart = [];
+  }
 
-type newItem ={
-    name:string,
-    price:number,
-    description:string
-}
+  addToCart(item: Item): void {
+    this.cart.push(item);
+  }
 
-class newItem{
-    name(){console.log("name")}
-    price(){console.log()}
-    description(){console.log("description")} 
+  printCart(): void {
+    console.log(this.name , this.cart);
+  }
+//                            removed: quantity in below arguement bc need counter for cart
+  removeItemFromCart(item: Item): void {
+    this.cart = this.cart.filter((cartItem) => cartItem.id !== item.id);
+  }
+// need some sort of counter....
 }
+const milk = new Item('Milk', 3.99, 'Whole Milk');
+const apples = new Item('Apples', 5.99, '3lbs of Pink Lady Apples');
+const eggs = new Item('Eggs', 7.99, 'Free Range Farm Eggs');
+const bread = new Item('Bread', 4.99, 'Sourdough');
 
-function createItem(newitem:unknown){
-    if (newitem instanceof newItem){
-        newitem.name()
-        newitem.price()
-        newitem.description()
-    }else{
-        console.log("please add new item")
-    }
-}
 
-type addTo={
-    //     object of item type 
-    //     object of User
-}
+const user1 = new User('Beth Ross', 25);
+const user2 = new User('Jack Black', 55);
 
-// need a way to count & add(+)
-function addToCart(addto:unknown){
-    if (addto instanceof addTo){
-//     object of item type 
-//     object of User        
-    }else{
-        console.log("add something to cart")
-    }
-}
 
-type removeFrom={
-    //     object of item type 
-    //     object of User       
-}
-class removeFrom{
-    //     object of item type 
-    //     object of User       
-}
-// need a way to count & subtract(-)
-function removeFromCart(removefrom:unknown){
-    if (removefrom instanceof removeFrom){
-//     object of item type 
-//     object of User   
-    }else{
-        console.log("do you want to remove item?")
-    }
-}
+// need to add quantity counter in cell above, then add extra argument to below
+user1.addToCart(milk);
+user1.addToCart(bread);
+// user1.addToCart(apples);
+user1.addToCart(eggs);
 
-// Step 4: Create Driver Code to emulate a front end user
-// Create 3 Items to Sell. For this example we will call them Item A Item B and Item C (you can name them anything that make sense)
-// Add Item A to the users Cart
-// print the contents of the user's cart
-// print the Total of the user's cart
-// Add 3 Items to the users Cart
-// print the contents of the user's cart
-// print the Total of the user's cart
-// Use your remove (not remove by Quantity function) to remove all of Items from your cart
-// print the contents of the user's cart
-// print the Total of the user's cart
+user1.printCart();
+user2.addToCart(milk);
+user2.addToCart(apples);
+user2.addToCart(bread);
+
+user2.printCart();
+
+user1.removeItemFromCart(eggs);
+user1.printCart();
+
+// need a way to have quantity
+// user1.removeQuantityFromCart(eggs);
+// user1.printCart();
